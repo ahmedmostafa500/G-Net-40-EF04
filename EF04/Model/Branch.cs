1d8d78d018a9a4862470a22bd078d3c8eed2fe71
ahmedmostafa500 <plmncar500@gmail.com>
2026-04-23 00:12:35 +0200

    EF04

---
+++ EF04/Model/Branch.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF04.Model
+{
+    internal class Branch
+    {
+        [Key]
+        public int Code { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+
+        public Manager? Manager { get; set; }
+
+        public ICollection<Account> Accounts { get; set; }=new List<Account>();
+    }
+}
---
+++ EF04/Model/Manager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF04.Model
+{
+    internal class Manager
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public DateTime HireDate { get; set; }
+
+        public int BranchCode { get; set; } //FK
+        public Branch? Branch { get; set; }
+    }
+}
